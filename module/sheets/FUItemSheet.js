230e778a25f4b3adf2c5848863fbf71764e93846
Peppesau <giuseppesaullo7@gmail.com>
2023-01-26 18:32:21 +0100

    Fixed FUItemSheet data and modified template.
    Fixed FUItemSheet data and modified template.json. Fixed also some problems with weapon-sheet.

--- module/sheets/FUItemSheet.js
+++ module/sheets/FUItemSheet.js
@@ -1,17 +1,20 @@
 export default class FUItemSheet extends ItemSheet {
     get template(){       
-        return `systems/FU/templates/sheets/${this.item.data.type}-sheet.html`;
+        return `systems/fu/templates/sheets/${this.item.type}-sheet.html`;
     }
 
     getData() {
-        const baseData = super.getData(); 
+        const baseData = super.getData();
+        //console.log(baseData.item.data.data);
+
         let sheetData = {
             owner: this.item.isOwner,
             editable: this.isEditable,
             item: baseData.item,
-            data: baseData.item.data.data,
+            data: baseData.item.system,
             config: CONFIG.fu
         }
+
 
         return sheetData;
     }
