d19d7bc84668c48bdcea7b8cb792031f20d27a0d
Peppesau <giuseppesaullo7@gmail.com>
2023-01-26 15:14:43 +0100

    Added less module and gulpfile, modified desc.
    Added less module and gulpfile, modified description in weapon_sheet

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('less', function(cb) {
+  gulp
+    .src('*.less')
+    .pipe(less())
+    .pipe(gulp.dest("./"));
+      /*gulp.dest(function(f) {
+        return f.base;
+      })*/
+    //);
+  cb();
+});
+
+gulp.task(
+  'default',
+  gulp.series('less', function(cb) {
+    gulp.watch('*.less', gulp.series('less'));
+    cb();
+  })
+);
